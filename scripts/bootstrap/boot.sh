3fac68c191f9e626140c5bfb06ac6d9902b858d7
Scott Richardson <dev@grogwood.com>
2019-12-21 15:32:51 -0800

    Readme / comment updates

--- scripts/bootstrap/boot.sh
+++ scripts/bootstrap/boot.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 # This is the main Chef server side script.
-# It will install all required system packages, setup the databag secret key file, and then install Chef itself.
-# The secret key file to install is located at `./.chef/scripts/server/encrypted_data_bag_secret`.
-# That file should never be stored unencrypted in a git repo.
+# It will install all required system packages and then install Chef itself.
 
 # Get the current directory of this script
 DIR="${BASH_SOURCE%/*}"
